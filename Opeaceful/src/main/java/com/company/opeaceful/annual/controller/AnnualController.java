08d56d06ca3f80bb203414506d597e290eee897e
nohjieui <shwldml@gmail.com>
2023-05-25 13:44:29 +0900

    (지의) 채팅 아이콘 배치 - sidebar에 같이 있음

---
+++ Opeaceful/src/main/java/com/company/opeaceful/annual/controller/AnnualController.java
@@ -0,0 +1,23 @@
+package com.company.opeaceful.annual.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/annual")
+public class AnnualController {
+
+	@GetMapping("/setting")
+	public String settingAnnaul(){
+		return "annual";
+	}
+	
+	
+	@GetMapping("/list")
+	public String listAnnaul(){
+		return "annual-user";
+	}
+	
+	
+}
