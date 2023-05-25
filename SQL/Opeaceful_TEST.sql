9b6f09fdfba71addbb57fad9fd4a5f8e5fa04953
hsy3373 <hsy3373@naver.com>
2023-05-25 10:07:55 +0900

    (승은) sql 테스트 멤버 데이터 수정

--- SQL/Opeaceful_TEST.sql
+++ SQL/Opeaceful_TEST.sql
@@ -1,5 +1,5 @@
-﻿-- 멤버 테스트 데이터
+-- 멤버 테스트 데이터
 
-INSERT INTO MEMBER(ENO,STATUS_TYPE, USER_PWD, USER_NAME, HIRE_DATE)
+INSERT INTO MEMBER(ENO, STATUS_TYPE, USER_PWD, USER_NAME, HIRE_DATE, EMAIL)
+   VALUES ( 230502 , 0,'test', 'test', SYSDATE(), 'test@gmail.com');
 
-   VALUES ( '230501' ,0, 'test', 'test', SYSDATE());
